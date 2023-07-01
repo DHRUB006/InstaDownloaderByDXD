8929f4600f57a714f62a1a6f5085ed23defdc2f4
-DHRUB_DXD <106004326+DHRUB006@users.noreply.github.com>
2023-07-01 15:41:26 +0530

    Update download.py

--- download.py
+++ download.py
@@ -13,11 +13,26 @@ def download():
     try:
         # Send a request to download the media
         response = requests.get(url, stream=True)
-        # Add your logic here to save the media file
 
-        return jsonify({'success': True})
-    except:
-        return jsonify({'success': False})
+        if response.status_code == 200:
+            # Extract the file extension from the response headers
+            content_type = response.headers['content-type']
+            extension = content_type.split('/')[-1]
+
+            # Generate a filename for the downloaded media
+            filename = f"{media_id}.{extension}"
+
+            # Save the media file
+            with open(filename, 'wb') as f:
+                for chunk in response.iter_content(1024):
+                    f.write(chunk)
+
+            return jsonify({'success': True, 'filename': filename})
+        else:
+            return jsonify({'success': False, 'error': 'Failed to download media'})
+
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
     app.run()
